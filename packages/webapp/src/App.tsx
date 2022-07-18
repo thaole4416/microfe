12c70532eee8c639f24ccac1b7eed7e852eacc48
Thao Le <thaole4416@gmail.com>
2022-07-18 11:49:16 +0700

    initial commit

---
+++ packages/webapp/src/App.tsx
@@ -0,0 +1,34 @@
+import Header from '@microfe/menu';
+import { useEffect, useState } from 'react';
+import { GlobalStore } from 'redux-micro-frontend';
+const App = () => {
+  const [price, setPrice] = useState(0);
+  const globalStore = GlobalStore.Get();
+  const appStore = globalStore.CreateStore('webapp', (state: any, action: any) => {
+    switch (action.type) {
+      case 'setPrice':
+        return action.payload;
+      default:
+        return 1;
+    }
+  });
+  globalStore.RegisterStore('webapp', appStore);
+  globalStore.RegisterGlobalActions('webapp', ['setPrice']);
+  globalStore.Subscribe('webapp', price => {
+    setPrice(price);
+  });
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('set Price 10');
+      globalStore.DispatchAction('setPrice', { type: 'setPrice', payload: 10 });
+    }, 2000);
+  }, []);
+  return (
+    <>
+      <Header.Header />
+      Posi price is {price}
+    </>
+  );
+};
+
+export default App;
